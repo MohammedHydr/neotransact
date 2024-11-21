16c16f8c5b9e379f1b39f3a63191ec017d927861
Mohammed Haydar <mohammed.haidar.1999@hotmail.com>
2024-11-21 23:51:08 +0200

    Correct commit without .idea

---
+++ scripts/generate_clients.py
@@ -0,0 +1,66 @@
+import pandas as pd
+from faker import Faker
+import random
+from datetime import timedelta
+
+# Lebanese DATA
+fake = Faker()
+
+"""
+The `Faker` library is a Python package used to generate synthetic data for testing and prototyping. 
+In this context, we use `Faker` to create realistic client information, such as names, emails, dates of 
+birth, phone numbers, and addresses. This is particularly useful when no real data is available, as it 
+allows us to simulate a production-like dataset without violating privacy concerns.
+
+Additionally, we enhance realism by pairing `Faker` with Python’s `random` module to assign 
+attributes such as account types, risk tolerances, and account balances based on weighted 
+probabilities, ensuring more realistic and diverse synthetic data.
+"""
+
+
+def generate_clients(num_clients):
+    clients = []
+    for _ in range(num_clients):
+        # make wieghts realistic
+        account_type = random.choices(["standard", "premium", "corporate"], weights=[60, 30, 10], k=1)[0]
+        # match type with balance also for more realistic
+        account_balance = {
+            "standard": round(random.uniform(1000, 50000), 2),
+            "premium": round(random.uniform(50001, 200000), 2),
+            "corporate": round(random.uniform(200001, 1000000), 2)}[account_type]
+        risk_tolerance = {
+            "standard": "low",
+            "premium": "medium",
+            "corporate": "high"}[account_type]
+        client_id = fake.uuid4()[:8]
+        name = fake.name()
+        email = fake.email()
+        date_of_birth = fake.date_of_birth(minimum_age=18, maximum_age=80)
+        account_open_date = fake.date_between(start_date=date_of_birth + timedelta(days=6570))  # >dob
+        country = fake.country()
+        status = random.choices(["active", "inactive", "suspended"], weights=[85, 10, 5], k=1)[0]
+        phone_number = fake.phone_number()
+        address = fake.address()
+        # risk_tolerance = random.choice(["low", "medium", "high"])
+        account_currency = random.choice(["USD", "EUR", "GBP"])
+
+        clients.append({
+            "client_id": client_id,
+            "name": name,
+            "email": email,
+            "date_of_birth": date_of_birth,
+            "country": country,
+            "account_type": account_type,
+            "account_balance": account_balance,
+            "account_open_date": account_open_date,
+            "status": status,
+            "phone_number": phone_number,
+            "address": address,
+            "risk_tolerance": risk_tolerance,
+            "account_currency": account_currency
+        })
+    return pd.DataFrame(clients)
+
+
+clients_df = generate_clients(100)
+clients_df.to_csv("../data/clients.csv", index=False)
---
+++ scripts/generate_transactions.py
@@ -0,0 +1,70 @@
+import pandas as pd
+from faker import Faker
+import random
+from datetime import datetime
+
+fake = Faker()
+
+# GET CLIENTS
+clients_df = pd.read_csv("../data/clients.csv")
+clients = clients_df.to_dict('records')
+
+STOCKS = [{"ticker": "AAPL", "name": "Apple Inc."}, {"ticker": "TSLA", "name": "Tesla Inc."},
+          {"ticker": "AMZN", "name": "Amazon.com Inc."}, {"ticker": "GOOGL", "name": "Alphabet Inc."},
+          {"ticker": "MSFT", "name": "Microsoft Corp."}, {"ticker": "NVDA", "name": "NVIDIA Corp."}]
+
+
+def generate_transactions(num_transactions):
+    transactions = []
+    for _ in range(num_transactions):
+        client = random.choice(clients)
+        client_id = client["client_id"]
+        account_type = client["account_type"]
+
+        # match amount with type
+        if account_type == "standard":
+            price = round(random.uniform(10, 200), 2)
+            shares = random.randint(1, 50)
+        elif account_type == "premium":
+            price = round(random.uniform(50, 500), 2)
+            shares = random.randint(1, 100)
+        else:
+            price = round(random.uniform(100, 1500), 2)
+            shares = random.randint(10, 500)
+
+        # account_creation_date < transaction_date < today
+        account_creation_date = datetime.strptime(client["account_open_date"], "%Y-%m-%d")
+        transaction_date = fake.date_time_between_dates(datetime_start=account_creation_date,
+                                                        datetime_end=datetime.today())
+        transaction_date = transaction_date.replace(tzinfo=None)
+        stock = random.choice(STOCKS)
+        # shares = random.randint(1, 100)
+        # price = round(random.uniform(10, 1500), 2)
+        transaction_id = fake.uuid4()[:8]
+        transaction_type = random.choice(["buy", "sell"])
+        amount = round(shares * price, 2)
+        if transaction_type == "sell":
+            amount = -amount  # sell => -ve
+        currency = random.choice(["USD", "EUR", "GBP"])
+        transaction_status = random.choice(["completed", "pending", "failed"])
+        transaction_channel = random.choice(["online", "branch", "ATM"])
+
+        transactions.append({
+            "transaction_id": transaction_id,
+            "client_id": client_id,
+            "transaction_type": transaction_type,
+            "transaction_date": transaction_date,
+            "stock_ticker": stock["ticker"],
+            "stock_name": stock["name"],
+            "shares": shares,
+            "price": price,
+            "amount": amount,
+            "currency": currency,
+            "transaction_status": transaction_status,
+            "transaction_channel": transaction_channel
+        })
+    return pd.DataFrame(transactions)
+
+
+transactions_df = generate_transactions(3000)
+transactions_df.to_excel("../data/transactions.xlsx", index=False)
